22baa1b8e9260c492526ee98ef9e71c25ab0b560
AniutaP <musenok.ko@mail.ru>
2023-12-02 01:52:41 +0300

    update

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -147,7 +147,8 @@ LOCALE_PATHS = [
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STORAGES = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
