d931992fe73f3f1a146d287989f58f12dff285c1
AniutaP <musenok.ko@mail.ru>
2023-12-02 01:19:22 +0300

    update

--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import get_user_model
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from django.contrib.messages.views import SuccessMessageMixin
-from .forms import UserForm
+from task_manager.users.forms import UserForm
 from django.urls import reverse_lazy
 from django.utils.translation import gettext_lazy as _
 from task_manager.mixins import AuthenticateMixin, PermissionMixin, DeleteProtectionMixin
